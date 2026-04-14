1fa1ee650baeaca779e01bd47582de91f6e76943
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-14 08:45:27 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/quartz/quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Shennan's Notes",
+    pageTitleSuffix: " | Shennan",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "codebyshennan.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
