828809db8c773cfc1eeb35102f21493c517edaf7
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-16 15:32:53 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/quartz/quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Shennan's Notes",
-    pageTitleSuffix: " | Shennan",
+    pageTitle: "Shen Nan's Notes",
+    pageTitleSuffix: " | Shen Nan",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
