6ce3c42779384c14f2ddd610af5fe4b9e4227f82
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-17 16:26:26 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/quartz/quartz/components/Body.tsx

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,13 +1,16 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
+// @ts-ignore
+import codeLanguageScript from "./scripts/code-language.inline"
+import codeLanguageStyle from "./styles/code-language.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript
-Body.css = clipboardStyle
+Body.afterDOMLoaded = [clipboardScript, codeLanguageScript]
+Body.css = [clipboardStyle, codeLanguageStyle]
 
 export default (() => Body) satisfies QuartzComponentConstructor
